0e04ccc83eb4821f4772db335d871d0caa6df631
Dasa <153184061+Dasa122@users.noreply.github.com>
2025-09-19 21:49:33 +0200

    Refactor input configuration, enhance Waybar styling, and add hyprlock configuration

--- hyprrepair.sh
+++ hyprrepair.sh
@@ -1,5 +1,5 @@
 sudo pacman -Rns $(pacman -Q | grep -E '(hypr|aqua)'| awk '{print $1}') --noconfirm
 cd ~/Hyprland
 git pull
-paru -S ninja gcc cmake meson libxcb xcb-proto xcb-util xcb-util-keysyms libxfixes libx11 libxcomposite libxrender libxcursor pixman wayland-protocols cairo pango libxkbcommon xcb-util-wm xorg-xwayland libinput libliftoff libdisplay-info cpio tomlplusplus hyprlang-git hyprcursor-git hyprwayland-scanner-git xcb-util-errors hyprutils-git glaze hyprgraphics-git aquamarine-git re2 hyprland-qtutils --noconfirm
+paru -S ninja gcc cmake meson libxcb xcb-proto xcb-util xcb-util-keysyms libxfixes libx11 libxcomposite libxrender libxcursor pixman wayland-protocols cairo pango libxkbcommon xcb-util-wm xorg-xwayland libinput libliftoff libdisplay-info cpio tomlplusplus hyprlang-git hyprcursor-git hyprwayland-scanner-git xcb-util-errors hyprutils-git glaze hyprgraphics-git aquamarine-git re2 hyprland-qtutils
 make all && sudo make install
